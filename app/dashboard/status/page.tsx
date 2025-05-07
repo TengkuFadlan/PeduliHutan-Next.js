e6f684be9a1944a1a030ca9c0bcf817ed233af1c
TengkuFadlan <fadlan.praditya02@gmail.com>
2025-05-07 18:03:16 +0700

    DashboardStatus Page

--- app/dashboard/status/page.tsx
+++ app/dashboard/status/page.tsx
@@ -1,8 +1,49 @@
+import { Badge, Box, Card, Flex, Grid, Heading, Progress, Separator } from '@radix-ui/themes'
 import React from 'react'
 
 const DashboardStatusPage = () => {
   return (
-    <div>DashboardStatus</div>
+    <Box p="5">
+      <Flex direction="column" align="center" justify="center" gap="3">
+
+        <Box width="50vw">
+          <Card>
+            <Heading>
+              Gambar Terakhir
+            </Heading>
+            <Separator my="1" size="4" />
+            <img width="100%" src="https://wallpaperaccess.com/full/1371397.jpg"></img>
+          </Card>
+        </Box>
+
+
+        <Grid columns="2" gap="3">
+          <Card size="2">
+            <Heading>
+              Status Hutan
+            </Heading>
+            <Separator my="1" size="4" />
+            <Badge color="green" size="3">Aman</Badge>
+          </Card>
+
+          <Card size="2">
+            <Heading>
+              Status Baterai
+            </Heading>
+            <Separator my="1" size="4" />
+            <Progress value={75} size="3" color="green" />
+          </Card>
+
+          <Card size="2">
+            <Heading>
+              Status Air
+            </Heading>
+            <Separator my="1" size="4" />
+            <Progress value={90} size="3" color="blue" />
+          </Card>
+        </Grid>
+      </Flex>
+    </Box>
   )
 }
 
